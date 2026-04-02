b806bdad6470945930bff770c05a8b08d0e0f686
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-02 09:47:55 +0600

    initial commit

---
+++ Need-to-know.asm
@@ -0,0 +1,11 @@
+; Character input	---> AL
+; Character output	---> DL
+; Function select	---> AH
+; Full 16-bit data	---> AX
+; String address	---> DX
+; Data access	    ---> DS
+; Code location	    ---> CS    
+; base address      ---> BX
+; 8-bit data        ---> BL
+; Loop counter      ---> CX
+; Shift count       ---> CL
