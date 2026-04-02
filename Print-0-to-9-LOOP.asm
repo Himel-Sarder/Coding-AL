8f717eec4bc9d0c8b03fe6cda09d74eeaa573241
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-02 17:46:37 +0600

    initial commit
    Example: 
    NUMBER FROM 0 TO 9:  
    0123456789

---
+++ Print-0-to-9-LOOP.asm
@@ -0,0 +1,39 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+    MSG1 DB 'NUMBER FROM 0 TO 9: $'
+    MSG2 DB 0DH, 0AH, '$'
+.CODE
+
+MAIN PROC
+    
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    LEA DX, MSG1
+    MOV AH, 09H
+    INT 21H
+    
+    LEA DX, MSG2
+    MOV AH, 09H
+    INT 21H
+    
+    MOV CX, 10
+    MOV DL, 48
+    
+    L1:  
+    
+    MOV AH, 2
+    INT 21H
+    
+    ADD DL, 1
+    
+    LOOP L1 
+    
+    MOV AH, 4CH
+    INT 21H
+    
+    
+MAIN ENDP
+END MAIN
+; Copywrite 2026 Himel Sarder. Feel free to use and learn, but don’t forget to give credit.
