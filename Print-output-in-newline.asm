b25f1682974676f88038305494f182aef4f91ef3
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-02 15:54:30 +0600

    initial commit

--- Print-output-in-newline.asm
+++ Print-output-in-newline.asm
@@ -38,3 +38,43 @@ MAIN ENDP
 END MAIN
 
 ; Copywrite 2026 Himel Sarder. Feel free to use and learn, but don’t forget to give credit.
+; ------------------------------------------------------------------------------------------
+; Using EQU
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+    char DB ?          ; Store input character
+
+CR   EQU 0DH           ; Carriage Return
+LF   EQU 0AH           ; Line Feed
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; Read character from user (with echo)
+    MOV AH, 01H
+    INT 21H
+    MOV char, AL        ; Save input in variable
+
+    ; Print newline first (CR + LF)
+    MOV DL, CR
+    MOV AH, 02H
+    INT 21H
+
+    MOV DL, LF
+    MOV AH, 02H
+    INT 21H
+
+    ; Print the character again
+    MOV DL, char
+    MOV AH, 02H
+    INT 21H
+
+    ; Exit program
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
