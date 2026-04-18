69984aa0b68661fdc024715578b9712628ed6b80
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-18 18:44:59 +0600

    initial commit

--- Ytha Yu - Assembly Language Programming and Organization of the IBM PC/Chapter 06/Programming Exercises/Ex11.asm
+++ Ytha Yu - Assembly Language Programming and Organization of the IBM PC/Chapter 06/Programming Exercises/Ex11.asm
@@ -1,124 +1,94 @@
+; Do programming exercise 10, except that if the user fails to enter a hex-digit character in three tries, display a message and terminate the program.
+
 .MODEL SMALL
 .STACK 100H
 
 .DATA
-    PR1 DB 0DH,0AH,'ENTER A HEX DIGIT: $'
-    PR2 DB 0DH,0AH,'DO YOU WANT TO DO IT AGAIN? $'
-    PR3 DB 0DH,0AH,'ILLEGAL CHARACTER - ENTER 0..9 OR A..F $'
-    PR4 DB 0DH,0AH,'IN DECIMAL IS IT: $'
-    PR5 DB 0DH,0AH,'TOO MANY INVALID ATTEMPTS. PROGRAM TERMINATED.$'
+    PROMPT DB 0DH, 0AH, 'ENTER A HEX DIGIT: $'
+    MSG_DEC DB 0DH, 0AH, 'IN DECIMAL IT IS: $'
+    ERR_MSG DB 0DH, 0AH, 'ILLEGAL CHARACTER! TRY AGAIN.$'
+    FAIL_MSG DB 0DH, 0AH, 'THREE WRONG TRIES. PROGRAM TERMINATED.$'
+    TRY_COUNT DB 0 ; ভুল ইনপুটের সংখ্যা রাখার জন্য
 
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-    MOV CL, 0       ; error counter = 0
-
-START:
-    ; ---- INPUT ----
-    LEA DX, PR1
-    MOV AH, 09H
+START_INPUT:
+    ; প্রম্পট দেখানো
+    LEA DX, PROMPT
+    MOV AH, 9
     INT 21H
 
-    MOV AH, 01H
+    ; ইনপুট নেওয়া
+    MOV AH, 1
     INT 21H
+    MOV BL, AL ; ইনপুট BL এ সেভ করা
 
-    ; ---- lowercase to uppercase ----
-    CMP AL, 'a'
-    JL CHECK
-    CMP AL, 'f'
-    JG CHECK
-    SUB AL, 20H    
-
-CHECK:
-    ; ---- VALIDATION ----
-    CMP AL, '0'
+    ; ভ্যালিডেশন শুরু (0-9 কি না)
+    CMP BL, '0'
     JL INVALID
-    CMP AL, '9'
-    JLE CONVERT
-
-    CMP AL, 'A'
-    JL INVALID
-    CMP AL, 'F'
-    JG INVALID
-
-CONVERT:
-    MOV CL, 0       ; reset error counter after valid input
-    MOV BL, AL
-
-    ; ---- HEX TO DECIMAL ----
     CMP BL, '9'
-    JLE NUM
+    JBE IS_DIGIT
 
-    SUB BL, 'A'
-    ADD BL, 10
-    JMP PRINT
-
-NUM:
-    SUB BL, '0'
-
-PRINT:
-    LEA DX, PR4
-    MOV AH, 09H
-    INT 21H
-
-    MOV AL, BL
-    MOV AH, 0
-    MOV BL, 10
-    DIV BL
-
-    MOV BH, AH      ; remainder
-    MOV BL, AL      ; quotient
-
-    ; ---- print tens ----
-    CMP BL, 0
-    JE ONEDIGIT
-
-    ADD BL, 30H
-    MOV DL, BL
-    MOV AH, 02H
-    INT 21H
-
-ONEDIGIT:
-    MOV DL, BH
-    ADD DL, 30H
-    MOV AH, 02H
-    INT 21H
-
-    ; ---- AGAIN ----
-    LEA DX, PR2
-    MOV AH, 09H
-    INT 21H
-
-    MOV AH, 01H
-    INT 21H
-
-    CMP AL, 'Y'
-    JE START
-    CMP AL, 'y'
-    JE START
-
-    MOV AH, 4CH
-    INT 21H
+    ; A-F কি না
+    CMP BL, 'A'
+    JL INVALID
+    CMP BL, 'F'
+    JBE IS_HEX
+    
+    ; a-f কি না (ছোট হাতের অক্ষর হ্যান্ডেল করার জন্য)
+    CMP BL, 'a'
+    JL INVALID
+    CMP BL, 'f'
+    JBE IS_HEX_LOWER
 
 INVALID:
-    INC CL          ; increase error counter
-    CMP CL, 3
-    JE TERMINATE
+    INC TRY_COUNT ; ভুলের সংখ্যা ১ বাড়ানো
+    CMP TRY_COUNT, 3
+    JE FAILED ; ৩ বার ভুল হলে FAILED লেবেলে যাবে
 
-    LEA DX, PR3
-    MOV AH, 09H
+    LEA DX, ERR_MSG ; ভুল ইনপুটের মেসেজ
+    MOV AH, 9
     INT 21H
-    JMP START
+    JMP START_INPUT ; আবার ইনপুট নিতে ফেরত যাওয়া
 
-TERMINATE:
-    LEA DX, PR5
-    MOV AH, 09H
+IS_DIGIT:
+    LEA DX, MSG_DEC
+    MOV AH, 9
+    INT 21H
+    MOV DL, BL
+    MOV AH, 2
+    INT 21H
+    JMP EXIT
+
+IS_HEX:
+    SUB BL, 11H ; হেক্স টু ডেসিমেল লজিক
+    JMP PRINT_HEX
+
+IS_HEX_LOWER:
+    SUB BL, 31H ; ছোট হাতের a-f এর জন্য
+    JMP PRINT_HEX
+
+PRINT_HEX:
+    LEA DX, MSG_DEC
+    MOV AH, 9
+    INT 21H
+    MOV DL, '1' ; প্রথম ডিজিট '1'
+    MOV AH, 2
+    INT 21H
+    MOV DL, BL ; দ্বিতীয় ডিজিট
+    INT 21H
+    JMP EXIT
+
+FAILED:
+    LEA DX, FAIL_MSG ; ৩ বার ভুলের পর শেষ মেসেজ
+    MOV AH, 9
     INT 21H
 
+EXIT:
     MOV AH, 4CH
     INT 21H
-
 MAIN ENDP
 END MAIN
