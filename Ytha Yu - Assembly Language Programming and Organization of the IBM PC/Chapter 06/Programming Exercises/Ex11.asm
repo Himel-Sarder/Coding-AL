b73d8fee26a8e7242d7ca44dfe9bc34379cf75df
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-18 19:25:38 +0600

    initial commit

--- Ytha Yu - Assembly Language Programming and Organization of the IBM PC/Chapter 06/Programming Exercises/Ex11.asm
+++ Ytha Yu - Assembly Language Programming and Organization of the IBM PC/Chapter 06/Programming Exercises/Ex11.asm
@@ -4,18 +4,22 @@
 .STACK 100H
 
 .DATA
-    PROMPT DB 0DH, 0AH, 'ENTER A HEX DIGIT: $'
-    MSG_DEC DB 0DH, 0AH, 'IN DECIMAL IT IS: $'
-    ERR_MSG DB 0DH, 0AH, 'ILLEGAL CHARACTER! TRY AGAIN.$'
+    PROMPT   DB 0DH, 0AH, 'ENTER A HEX DIGIT: $'
+    MSG_DEC  DB 0DH, 0AH, 'IN DECIMAL IT IS: $'
+    ERR_MSG  DB 0DH, 0AH, 'ILLEGAL CHARACTER! TRY AGAIN.$'
     FAIL_MSG DB 0DH, 0AH, 'THREE WRONG TRIES. PROGRAM TERMINATED.$'
-    TRY_COUNT DB 0 ; ভুল ইনপুটের সংখ্যা রাখার জন্য
+    AGAIN_MSG DB 0DH, 0AH, 'DO YOU WANT TO TRY AGAIN? (Y/N): $'
+    TRY_COUNT DB 0
 
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-START_INPUT:
+START_OVER:
+    MOV TRY_COUNT, 0   ; নতুন করে শুরু করলে কাউন্টার ০ হবে
+
+INPUT_LOOP:
     ; প্রম্পট দেখানো
     LEA DX, PROMPT
     MOV AH, 9
@@ -24,35 +28,35 @@ START_INPUT:
     ; ইনপুট নেওয়া
     MOV AH, 1
     INT 21H
-    MOV BL, AL ; ইনপুট BL এ সেভ করা
+    MOV BL, AL
 
-    ; ভ্যালিডেশন শুরু (0-9 কি না)
+    ; ভ্যালিডেশন (0-9)
     CMP BL, '0'
     JL INVALID
     CMP BL, '9'
     JBE IS_DIGIT
 
-    ; A-F কি না
+    ; ভ্যালিডেশন (A-F)
     CMP BL, 'A'
     JL INVALID
     CMP BL, 'F'
     JBE IS_HEX
     
-    ; a-f কি না (ছোট হাতের অক্ষর হ্যান্ডেল করার জন্য)
+    ; ভ্যালিডেশন (a-f)
     CMP BL, 'a'
     JL INVALID
     CMP BL, 'f'
     JBE IS_HEX_LOWER
 
 INVALID:
-    INC TRY_COUNT ; ভুলের সংখ্যা ১ বাড়ানো
+    INC TRY_COUNT
     CMP TRY_COUNT, 3
-    JE FAILED ; ৩ বার ভুল হলে FAILED লেবেলে যাবে
+    JE FAILED          ; ৩ বার ভুল হলে সরাসরি টার্মিনেট হবে
 
-    LEA DX, ERR_MSG ; ভুল ইনপুটের মেসেজ
+    LEA DX, ERR_MSG
     MOV AH, 9
     INT 21H
-    JMP START_INPUT ; আবার ইনপুট নিতে ফেরত যাওয়া
+    JMP INPUT_LOOP     ; আবার ইনপুট নিতে ফেরত যাওয়া
 
 IS_DIGIT:
     LEA DX, MSG_DEC
@@ -61,31 +65,44 @@ IS_DIGIT:
     MOV DL, BL
     MOV AH, 2
     INT 21H
-    JMP EXIT
+    JMP ASK_USER       ; সঠিক ইনপুটে জিজ্ঞাসা করবে আবার করবে কি না
 
 IS_HEX:
-    SUB BL, 11H ; হেক্স টু ডেসিমেল লজিক
+    SUB BL, 11H        ; ASCII অ্যাডজাস্টমেন্ট
     JMP PRINT_HEX
 
 IS_HEX_LOWER:
-    SUB BL, 31H ; ছোট হাতের a-f এর জন্য
-    JMP PRINT_HEX
+    SUB BL, 31H        ; ছোট হাতের অক্ষরের জন্য
 
 PRINT_HEX:
     LEA DX, MSG_DEC
     MOV AH, 9
     INT 21H
-    MOV DL, '1' ; প্রথম ডিজিট '1'
+    MOV DL, '1'        ; প্রথম ডিজিট ১
     MOV AH, 2
     INT 21H
-    MOV DL, BL ; দ্বিতীয় ডিজিট
+    MOV DL, BL         ; দ্বিতীয় ডিজিট
     INT 21H
-    JMP EXIT
+    JMP ASK_USER       ; সঠিক ইনপুটে জিজ্ঞাসা করবে আবার করবে কি না
 
 FAILED:
-    LEA DX, FAIL_MSG ; ৩ বার ভুলের পর শেষ মেসেজ
+    LEA DX, FAIL_MSG
     MOV AH, 9
     INT 21H
+    JMP EXIT           ; ৩ বার ভুল করলে সরাসরি এক্সিট (কোনো প্রশ্ন ছাড়া)
+
+ASK_USER:
+    LEA DX, AGAIN_MSG
+    MOV AH, 9
+    INT 21H
+
+    MOV AH, 1
+    INT 21H
+    
+    CMP AL, 'Y'
+    JE START_OVER
+    CMP AL, 'y'
+    JE START_OVER
 
 EXIT:
     MOV AH, 4CH
